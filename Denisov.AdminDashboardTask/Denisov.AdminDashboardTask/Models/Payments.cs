e25b714fa750e3a38f22889590e49041e1a7e598
Viktor <vitalineage22@gmail.com>
2025-06-17 16:15:35 +0300

    init: proj with models added

---
+++ Denisov.AdminDashboardTask/Denisov.AdminDashboardTask/Models/Payments.cs
@@ -0,0 +1,12 @@
+﻿namespace Denisov.AdminDashboardTask.Models
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; } = null!;
+        public virtual Client Client { get; set; } // Связь one-to-many с Client
+    }
+}
